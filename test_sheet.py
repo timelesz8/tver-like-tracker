9f74e9a73f43099ef5c02f3e35943bfc004d116f
timelesz8 <miki.yamamoto.jp@gmail.com>
2026-03-09 22:32:14 +0900

    Add files via upload

---
+++ test_sheet.py
@@ -0,0 +1,20 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_file(
+    "credentials.json",
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("スプレッドシート名").sheet1
+
+sheet.update("A1", "test")
+
+print("success")
